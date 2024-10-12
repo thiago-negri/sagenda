1bcb55ee166bcf97b9a8a3f68bdfd952f5cbd752
Thiago Negri <evohunz@gmail.com>
2024-10-12 12:07:23 -0300

    Code clean up

--- src/Sagenda/Database.hs
+++ src/Sagenda/Database.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Sagenda.Database (connectDatabase) where
 
 import qualified Data.ByteString.UTF8 as BSU
@@ -9,44 +7,36 @@ import Sagenda.Error (AppError (MissingEnvVar, DatabaseConnectionError))
 
 connectDatabase :: IO (Either AppError Connection)
 connectDatabase = do
-    connectionSettings <- connectionSettingsFromEnv
+    connectionSettings <- connectionSettingsEnv
     either (return . Left) acquire' connectionSettings
 
-connectionSettingsFromEnv :: IO (Either AppError Settings)
-connectionSettingsFromEnv = do
-    host' <- lookEnv "DB_HOST"
+connectionSettingsEnv :: IO (Either AppError Settings)
+connectionSettingsEnv = do
+    host' <- getEnvBS "DB_HOST"
     port' <- readEnv "DB_PORT"
-    user' <- lookEnv "DB_USER"
-    password' <- lookEnv "DB_PASSWORD"
-    database' <- lookEnv "DB_DATABASE"
-    return $ unpackSettingsFromEnv host' port' user' password' database'
+    user' <- getEnvBS "DB_USER"
+    password' <- getEnvBS "DB_PASSWORD"
+    database' <- getEnvBS "DB_DATABASE"
+    return $ unpackSettings host' port' user' password' database'
 
-lookEnv :: String -> IO (Either AppError ByteString)
-lookEnv varName = do
-    val' <- lookupEnv varName
-    let val = BSU.fromString <$> val'
-    maybe err (return . Right) val
-    where err = return $ Left (MissingEnvVar varName)
+getEnvBS :: String -> IO (Either AppError ByteString)
+getEnvBS = lookEnv (BSU.fromString <$>)
 
 readEnv :: Read a => String -> IO (Either AppError a)
-readEnv varName = do
-    val' <- lookupEnv varName
-    let val = readMaybe =<< val'
-    maybe err (return . Right) val
-    where err = return $ Left (MissingEnvVar varName)
+readEnv = lookEnv (readMaybe =<<)
 
-unpackSettingsFromEnv :: Either AppError ByteString ->
-                         Either AppError Word16 ->
-                         Either AppError ByteString ->
-                         Either AppError ByteString ->
-                         Either AppError ByteString ->
-                         Either AppError Settings
-unpackSettingsFromEnv host' port' user' password' database' =
+lookEnv :: (Maybe String -> Maybe a) -> String -> IO (Either AppError a)
+lookEnv f e = lookupEnv e >>= maybe err (return . Right) . f
+    where err = return $ Left (MissingEnvVar e)
+
+unpackSettings :: Either AppError ByteString ->
+                  Either AppError Word16 ->
+                  Either AppError ByteString ->
+                  Either AppError ByteString ->
+                  Either AppError ByteString ->
+                  Either AppError Settings
+unpackSettings host' port' user' password' database' =
     settings <$> host' <*> port' <*> user' <*> password' <*> database'
 
 acquire' :: Settings -> IO (Either AppError Connection)
-acquire' s = do
-    c' <- acquire s
-    return $ case c' of
-        Left e -> Left $ DatabaseConnectionError e
-        Right c -> Right c
+acquire' s = mapLeft DatabaseConnectionError <$> acquire s
